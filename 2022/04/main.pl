fcb517249800a35f3380b0bcf66c94055e33c68f
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-12-03 21:42:18 -0800

    Add solution for day 4

---
+++ 2022/04/main.pl
@@ -0,0 +1,54 @@
+% Oops, accidentally copied the input data twice and had 2000 rows instead of
+% 1000. Finding the overlapping intervals was actually a little tricky. I ended
+% up taking a different approach for part 2 and used set membership instead of
+% doing arithmetic calculations because the logic is way easier to understand
+% this way. 21 minutes for part one and 11 minutes for part two.
+
+sample("2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8").
+
+parse_input(In, Out):-
+    split_string(In, "\n", "", Split),
+    % now split each line into two pairs
+    maplist([Line, [A-B, C-D]]>>(
+            split_string(Line, ",-", "", Items),
+            % convert into numbers
+            maplist(atom_number, Items, [A, B, C, D])
+        ),
+        Split,
+        Out
+    ).
+
+overlap([A-B, C-D]):- (A =< C, B >= D; C =< A, D >= B).
+
+part1(In, Out):-
+    parse_input(In, Parsed),
+    include(overlap, Parsed, Valid),
+    length(Valid, Out).
+
+any_overlap([A-B, C-D]):-
+    between(A, B, X),
+    between(C, D, X).
+
+part2(In, Out):-
+    parse_input(In, Parsed),
+    include(any_overlap, Parsed, Valid),
+    length(Valid, Out).
+
+:-
+    UseInput = true,
+    (UseInput ->
+        open("2022/04/input.txt", read, Str),
+        read_string(Str, _, In),
+        close(Str)
+    ;
+        sample(In)
+    ),
+    part1(In, Out1),
+    print(Out1), nl,
+    part2(In, Out2),
+    print(Out2).
