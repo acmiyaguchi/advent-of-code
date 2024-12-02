c90ad67dd89323778a5459b07ca5673077ee2046
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2024-12-01 21:43:28 -0800

    initial p2

---
+++ 2024/02/main.pl
@@ -0,0 +1,78 @@
+sample("7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9").
+
+parse_line(Line, Out):-
+    split_string(Line, "\s", "\s", Split),
+    maplist(atom_number, Split, Out).
+
+parse_input(In, Out):-
+    split_string(In, "\n", "", Split),
+    maplist(parse_line, Split, Out).
+
+% base
+monotonically_increasing_safely(_,[_]).
+% safe
+monotonically_increasing_safely(ToleratedErrors, [A,B|T]):-
+    A < B,
+    abs(A-B) =< 3,
+    abs(A-B) >= 1,
+    monotonically_increasing_safely(ToleratedErrors, [B|T]).
+% tolerate an error by dropping the current element
+monotonically_increasing_safely(ToleratedErrors, [A,_|T]):-
+    ToleratedErrorsPrime is ToleratedErrors - 1,
+    ToleratedErrorsPrime >= 0,
+    monotonically_increasing_safely(ToleratedErrorsPrime, [A|T]).
+% tolerate an error by dropping the next element
+monotonically_increasing_safely(ToleratedErrors, [_,B|T]):-
+    ToleratedErrorsPrime is ToleratedErrors - 1,
+    ToleratedErrorsPrime >= 0,
+    monotonically_increasing_safely(ToleratedErrorsPrime, [B|T]).
+
+monotonically_decreasing_safely(_,[_]).
+monotonically_decreasing_safely(ToleratedErrors, [A,B|T]):-
+    A > B,
+    abs(A-B) =< 3,
+    abs(A-B) >= 1,
+    monotonically_decreasing_safely(ToleratedErrors, [B|T]).
+monotonically_decreasing_safely(ToleratedErrors, [A,_|T]):-
+    ToleratedErrorsPrime is ToleratedErrors - 1,
+    ToleratedErrorsPrime >= 0,
+    monotonically_decreasing_safely(ToleratedErrorsPrime, [A|T]).
+monotonically_decreasing_safely(ToleratedErrors, [_,B|T]):-
+    ToleratedErrorsPrime is ToleratedErrors - 1,
+    ToleratedErrorsPrime >= 0,
+    monotonically_decreasing_safely(ToleratedErrorsPrime, [B|T]).
+
+part1(In, Out):-
+    parse_input(In, Levels),
+    include([L]>>(
+        (
+            monotonically_increasing_safely(0, L);
+            monotonically_decreasing_safely(0, L)
+        )
+    ), Levels, Valid),
+    length(Valid, Out).
+
+part2(In, Out):-
+    parse_input(In, Levels),
+    include([L]>>(
+        (
+            monotonically_increasing_safely(1, L);
+            monotonically_decreasing_safely(1, L)
+        )
+    ), Levels, Valid),
+    % print(Valid), nl,
+    length(Valid, Out).
+
+read_file(Path, Out):- open(Path, read, Fp), read_string(Fp, _, Out), close(Fp).
+:-
+    UseInput = false,
+    Path = "2024/02/input.txt",
+    (UseInput -> read_file(Path, In); sample(In)),
+    % parse_input(In, Out), print(Out).
+    part1(In, Out1), print(Out1), nl,
+    part2(In, Out2), print(Out2), nl.
